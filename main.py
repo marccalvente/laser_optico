c0db575524b33c563cb002b784b9232f755e6b8b
Marc Calvente <marc.calvente.lozano@gmail.com>
2023-01-31 13:57:18 +0100

    Correcciones tras meeting cliente

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ with st.expander("Introduzca puntos de corte y distáncia entre arcos", expanded
     x2 = st.number_input("Punto 2 - Coordenada X")
     y2 = st.number_input("Punto 2 - Coordenada Y")
     d_arcos = st.number_input("Distáncia entre los arcos")
-    profundidad = st.number_input("Profundidad del corte")
+    profundidad = st.number_input("Cutting depth")
 
-ri = 20
+ri = 3
 
 st.text(" ")
 boton_calcular = st.button("Calcular")
@@ -29,16 +29,16 @@ st.text(" ")
 st.subheader("Parámetros para el primer corte")
 
 col1, col2, col3, col4 = st.columns([3, 2, 2, 2])
-col1.metric("Centro", f"({round(cxe, 2)}, {round(cye, 2)})")
-col2.metric("Radio", f"{round(re, 4)}")
-col3.metric("Ángulo de corte", f"{round(angulo_externo, 2)}")
-col4.metric("Profundidad", f"{profundidad}")
+col1.metric("Centro (mm)", f"({round(cxe, 2)}, {round(cye, 2)})")
+col2.metric("Radio (mm)", f"{round(re, 3)}")
+col3.metric("Side cut angle (º)", f"{round(angulo_externo, 2)}")
+col4.metric("Cutting depth (µm)", f"{profundidad*1000}")
 
 st.text(" ")
 st.subheader("Parámetros para el segundo corte")
 
 col1, col2, col3, col4 = st.columns([3, 2, 2, 2])
-col1.metric("Centro", f"({round(cxi, 2)}, {round(cyi, 2)})")
-col2.metric("Radio", f"{round(ri, 4)}")
-col3.metric("Ángulo de corte", f"{round(angulo_interno, 2)}")
-col4.metric("Profundidad", f"{profundidad}")
+col1.metric("Centro (mm)", f"({round(cxi, 2)}, {round(cyi, 2)})")
+col2.metric("Radio (mm)", f"{round(ri, 3)}")
+col3.metric("Side cut angle (º)", f"{round(angulo_interno, 2)}")
+col4.metric("Cutting depth (µm)", f"{profundidad*1000}")
